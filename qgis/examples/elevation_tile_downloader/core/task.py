2b659b9dbf6c2758b16294e23789ed24e449e6d9
nilsnolde <nils.nolde@gmail.com>
2021-09-09 15:50:08 +0200

    make code more compact; lint & style check

--- qgis/examples/elevation_tile_downloader/core/task.py
+++ qgis/examples/elevation_tile_downloader/core/task.py
@@ -1,117 +1,122 @@
-from qgis.core import (QgsTask,
-                       QgsMessageLog,
-                       Qgis,
-                       QgsRasterLayer,
-                       QgsProject,
-                       QgsNetworkAccessManager,
-                       QgsNetworkReplyContent,
-                       QgsGeometry
-                       )
-from PyQt5.QtCore import QFileInfo, QUrl, pyqtSignal
-from qgis.PyQt.QtNetwork import QNetworkRequest
-from typing import List
+from typing import List, Optional, Set
 from pathlib import Path
-import traceback
 import gzip
 from io import BytesIO
 
+from qgis.gui import QgisInterface
+from qgis.core import (
+    QgsTask,
+    QgsMessageLog,
+    Qgis,
+    QgsRasterLayer,
+    QgsProject,
+    QgsNetworkAccessManager,
+    QgsNetworkReplyContent,
+    QgsGeometry,
+)
+from qgis.utils import iface
+from qgis.PyQt.QtCore import QUrl
+from qgis.PyQt.QtWidgets import QMessageBox
+from qgis.PyQt.QtNetwork import QNetworkRequest
+
+iface: QgisInterface
+
 
 class DownloadTask(QgsTask):
-    next_tile = pyqtSignal(int)
-
-    def __init__(self, grid: List[QgsGeometry], output_dir, add=False):
+    def __init__(self, grid: Set[QgsGeometry], output_dir: Path):
         super().__init__("Tile download task", QgsTask.CanCancel)
         self.grid = grid
         self.output_dir = output_dir
-        self.exception = None
-        self.traceback = None
-        self.total = len(self.grid)
-        self.add = add
+
+        self.exception: Optional[Exception] = None
         self.tiles: List[Path] = []
-        self.nam = QgsNetworkAccessManager()
 
     def run(self):
-        QgsMessageLog.logMessage(
-            message='Started task {}'.format(self.description()),
-            level=Qgis.Info
-        )
+        """Do the work."""
+        nam = QgsNetworkAccessManager()
+
+        # Don't raise any error during processing, relay error reporting to self.finished()
         try:
-            for i, poly in enumerate(self.grid):
-                self.next_tile.emit(i)
-                x, y = poly.boundingBox().xMinimum(), poly.boundingBox().yMinimum()
-                self.prepare_dir(y, self.output_dir)
-                tile_name = '%s%02d%s%03d.hgt' % ('S' if y < 0 else 'N', abs(y), 'W' if x < 0 else 'E', abs(x))
-                dir_name = '%s%02d' % ('S' if y < 0 else 'N', abs(y))
-                url = QUrl(f"http://s3.amazonaws.com/elevation-tiles-prod/skadi/{dir_name}/{tile_name}.gz")
-                request_ = QNetworkRequest(url)
-                dest_directory = Path(self.output_dir, dir_name)
-                filepath = Path(dest_directory, tile_name)
-                if not filepath.is_file():
-                    reply: QgsNetworkReplyContent = self.nam.blockingGet(request_)
-                    length = int(reply.rawHeader(bytes('Content-Length'.encode())))
-                    stream = BytesIO(reply.content())
-                    with gzip.GzipFile(fileobj=stream, mode='rb') as gz:
-                        with open(filepath, 'wb') as f:
-                            processed = 0
-                            chunk_size = 1024 ** 2
-                            while True:
-                                chunk = gz.read(chunk_size)
-                                if not chunk:
-                                    break
-                                f.write(chunk)
-                                processed += chunk_size
-                                self.setProgress((processed / length) * 100)  # goes to 90-something percent real fast and then stays there for another couple of seconds or so...
-                else:
-                    QgsMessageLog.logMessage(
-                        message=f"Already downloaded tile {url.url()}", level=Qgis.Info
-                    )
-                self.tiles.append(filepath)
+            for i, bbox in enumerate(self.grid):
                 if self.isCanceled():
                     return False
+
+                # Get the filepath and create the output directory (if necessary)
+                x, y = (
+                    bbox.boundingBox().xMinimum(),
+                    bbox.boundingBox().yMinimum(),
+                )
+                hemisphere = "S" if y < 0 else "N"
+                dir_name = "%s%02d" % (hemisphere, abs(y))
+                directory = self.output_dir.joinpath(dir_name)
+                directory.mkdir(exist_ok=True)
+                tile_name = "%s%02d%s%03d.hgt" % (
+                    hemisphere,
+                    abs(y),
+                    "W" if x < 0 else "E",
+                    abs(x),
+                )
+                filepath = self.output_dir.joinpath(dir_name, tile_name)
+                self.tiles.append(filepath)
+                if filepath.is_file():
+                    QgsMessageLog.logMessage(
+                        message=f"Already downloaded tile {tile_name}",
+                        level=Qgis.Info,
+                    )
+                    continue
+
+                # get the new tile if it didn't exist
+                url = QUrl(
+                    f"http://s3.amazonaws.com/elevation-tiles-prod/skadi/{dir_name}/{tile_name}.gz"
+                )
+                request = QNetworkRequest(url)
+                reply: QgsNetworkReplyContent = nam.blockingGet(request)
+                # turn the reply into a file object and write the decompressed file to disk
+                with gzip.GzipFile(
+                    fileobj=BytesIO(reply.content()), mode="rb"
+                ) as gz:
+                    with open(filepath, "wb") as f:
+                        f.write(gz.read())
+
+                self.setProgress((i / len(self.grid)) * 100)
+
             return True
         except Exception as e:
             self.exception = e
-            self.traceback = traceback.format_exc()
             return False
 
     def finished(self, result):
-        if result:
+        """Postprocessing after the run() method."""
+        if self.isCanceled():
+            # if it was canceled by the user
             QgsMessageLog.logMessage(
-                message=f'Finished downloading tiles. {len(self.tiles)} tiles downloaded.',
-                level=Qgis.Info
+                message=f"Canceled download task. {len(self.tiles)} tiles downloaded.",
+                level=Qgis.Warning,
             )
-            if self.add:
-                self.add_to_map()
-        elif not result and self.exception:
-            QgsMessageLog.logMessage(
-                message=f'An exception occurred during downloading: {self.exception}\n'
-                        f'{self.traceback}',
-                level=Qgis.Warning
+            return
+        elif not result:
+            # if there was an error
+            QMessageBox.critical(
+                iface.mainWindow(),
+                "Tile download error",
+                f"The following error occurred:\n{self.exception.__class__.__name__}: {self.exception}",
             )
-        elif not result and not self.exception:
-            QgsMessageLog.logMessage(
-                message=f'Canceled download task. {len(self.tiles)} tiles downloaded.',
-                level=Qgis.Warning
-            )
+            return
 
-    @staticmethod
-    def prepare_dir(y, dest_dir):
-        dir_name = '%s%02d' % ('S' if y < 0 else 'N', abs(y))
-        directory = Path(dest_dir, dir_name)
-        Path.mkdir(directory, exist_ok=True)
+        iface.messageBar().pushMessage(
+            "Success",
+            f"Finished downloading {len(self.tiles)} tiles.",
+            level=Qgis.Success,
+        )
 
-    def add_to_map(self):
-        project = QgsProject.instance()
+        # add the rasters to the map canvas
         for tile_path in self.tiles:
-            finfo = QFileInfo(str(tile_path))
-            base_name = finfo.baseName()
-            path = finfo.filePath()
-            if base_name and path:
-                raster: QgsRasterLayer = QgsRasterLayer(path, base_name)
-                if not raster.isValid():
-                    QgsMessageLog.logMessage(
-                        message=f'Raster not valid: {str(tile_path)}',
-                        level=Qgis.Warning
-                    )
-                else:
-                    project.addMapLayer(raster)
+            if not tile_path.exists():
+                QgsMessageLog.logMessage(
+                    message=f"Tile at {tile_path.resolve()} was removed.",
+                    level=Qgis.Warning,
+                )
+                continue
+            QgsProject.instance().addMapLayer(
+                QgsRasterLayer(str(tile_path.resolve()), tile_path.stem)
+            )
